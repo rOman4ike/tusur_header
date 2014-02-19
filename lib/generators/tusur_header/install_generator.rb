18da0efb8c23b4f7f54732db06418f94cded062c
Evgeny Lapin <lev@openteam.ru>
2014-02-19 10:55:55 +0700

    rebase stub partial

--- lib/generators/tusur_header/install_generator.rb
+++ lib/generators/tusur_header/install_generator.rb
@@ -3,9 +3,9 @@ module TusurHeader
     class InstallGenerator < Rails::Generators::Base
 
       desc 'Copy TusurHeader default file'
-      source_root File.expand_path('../templates', __FILE__)
+      source_root File.expand_path('../../../../app/views/application', __FILE__)
 
-      def copy_partial
+      def create_partial
         copy_file '_tusur_header.html.erb', 'app/views/application/_tusur_header.html.erb'
       end
     end
