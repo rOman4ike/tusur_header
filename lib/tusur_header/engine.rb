b6d75e1d59433ca8d54bcd2807fc65e5a54eb7cf
Evgeny Lapin <lev@openteam.ru>
2014-02-19 15:57:36 +0700

    load sites from yaml

--- lib/tusur_header/engine.rb
+++ lib/tusur_header/engine.rb
@@ -1,5 +1,8 @@
 module TusurHeader
   class Engine < ::Rails::Engine
+    def self.sites
+      @sites ||= YAML.load_file(File.expand_path('../../../config/sites.yml', __FILE__))
+    end
   end
 end
 
