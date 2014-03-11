60d317076f823a7667c3f0b41e0c65e56c601ae1
Manin Iliy <bkmzvf@gmail.com>
2014-03-11 10:08:24 +0700

    Getting notification count

--- lib/tusur_header/engine.rb
+++ lib/tusur_header/engine.rb
@@ -7,6 +7,27 @@ module TusurHeader
     def self.sites
       @sites ||= YAML.load_file(File.expand_path('../../../config/sites.yml', __FILE__))
     end
+
+    config.after_initialize do
+      begin
+        Settings.define 'profile.url', :require => true
+
+        Settings.resolve!
+      rescue => e
+        puts "WARNING! #{e.message}"
+      end
+    end
+
+    config.to_prepare do
+      ActiveRecord::Base.class_eval do
+        def self.has_remote_notifications
+          define_method :get_notification_count do
+            response = RestClient.get("#{Settings['profile.url']}/api/notifications", :params => { :uid => self.uid }, :content_type => :json, :accept => :json)
+            response.body['notifications']
+          end
+        end
+      end
+    end
   end
 end
 
--- tusur_header.gemspec
+++ tusur_header.gemspec
@@ -23,6 +23,7 @@ Gem::Specification.new do |s|
   s.add_runtime_dependency 'compass-rails'
   s.add_runtime_dependency 'jquery-rails'
   s.add_runtime_dependency 'sass-rails', '>= 3.2'
+  s.add_runtime_dependency 'rest-client'
 
   s.rubyforge_project = s.name
 end
