05fc9458e3443227697e2f39ddf72a014a9772c2
tab <tukhfatullin@gmail.com>
2014-04-03 14:42:58 +0700

    Add generator for error pages

---
+++ lib/generators/tusur_header/error_pages_generator.rb
@@ -0,0 +1,21 @@
+module TusurHeader
+  module Generators
+    class ErrorPagesGenerator < Rails::Generators::Base
+
+      desc 'Generate 403 404 500 and 502 error pages'
+      source_root File.expand_path("../templates", __FILE__)
+
+      def copy_error_pages
+        copy_file "403.html", "public/403.html"
+        copy_file "404.html", "public/404.html"
+        copy_file "500.html", "public/500.html"
+        copy_file "502.html", "public/502.html"
+      end
+
+      def copy_assets
+        copy_file "error.css", "public/error.css"
+        copy_file "error.js", "public/error.js"
+      end
+    end
+  end
+end
