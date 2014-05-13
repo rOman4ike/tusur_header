5f54d88f441ba02ea9c60340817d6893797f0624
Evgeny Lapin <lev@openteam.ru>
2014-05-13 13:51:44 +0700

    reanimated dummy app

--- lib/tusur_header/engine.rb
+++ lib/tusur_header/engine.rb
@@ -9,34 +9,39 @@ module TusurHeader
       @sites ||= YAML.load_file(File.expand_path('../../../config/sites.yml', __FILE__))
     end
 
-    config.after_initialize do
-      begin
-        Settings.define 'profile.url', :require => true
+    if Rails.env.production?
 
-        Settings.resolve!
-      rescue => e
-        puts "WARNING! #{e.message}"
+      config.after_initialize do
+        begin
+          Settings.define 'profile.url', :require => true
+
+          Settings.resolve!
+        rescue => e
+          puts "WARNING! #{e.message}"
+        end
       end
-    end
 
-    config.to_prepare do
-      ActiveRecord::Base.class_eval do
-        def self.has_remote_profile
-          define_method :remote_profile do |kind|
-            response = RestClient.get("#{Settings['profile.url']}/api/#{kind}", :params => { :uid => self.uid }, :content_type => :json, :accept => :json)
-            JSON.parse(response)[kind]
-          end
+      config.to_prepare do
+        ActiveRecord::Base.class_eval do
+          def self.has_remote_profile
+            define_method :remote_profile do |kind|
+              response = RestClient.get("#{Settings['profile.url']}/api/#{kind}", :params => { :uid => self.uid }, :content_type => :json, :accept => :json)
+              JSON.parse(response)[kind]
+            end
 
-          define_method :get_notifications do
-            @get_notification_count ||= remote_profile('notifications').to_i
-          end
+            define_method :get_notifications do
+              @get_notification_count ||= remote_profile('notifications').to_i
+            end
 
-          define_method :get_menu do
-            @get_menu ||= (remote_profile('menu') || [])
+            define_method :get_menu do
+              @get_menu ||= (remote_profile('menu') || [])
+            end
           end
         end
       end
+
     end
+
   end
 end
 
